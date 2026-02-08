56d109634fc455b7c6abcf467707ef3a19c1301b
Gabriel Buzzi Sanches <gabrielbuzzi2@gmail.com>
2026-02-08 17:35:46 -0300

    Saving train history together with weights checkpoint

--- src/grokking/main.py
+++ src/grokking/main.py
@@ -1,11 +1,14 @@
 import functools as ft
 import itertools as it
+import json
 import logging
+from pathlib import Path
 
 import hydra
-from pathlib import Path
 import jax
 import jax.numpy as jnp
+import orbax.checkpoint as ocp
+from flax.training import orbax_utils
 from matplotlib import pyplot as plt
 from omegaconf import DictConfig, OmegaConf
 from tqdm import tqdm
@@ -16,17 +19,16 @@ from grokking.data.tokenizer import Tokenizer
 from grokking.model.loss import cross_entropy_loss
 from grokking.model.parameters import init_param_state
 from grokking.model.transformer import decoder_only
-from grokking.model.utils import evaluate
-import orbax.checkpoint as ocp
-from flax.training import orbax_utils
+from grokking.model.utils import accuracy, evaluate, one_hot_encode
 
 log = logging.getLogger(__name__)
-logging.getLogger('orbax').setLevel(logging.WARNING)
+logging.getLogger("orbax").setLevel(logging.WARNING)
+plt.style.use("config/plotstyle.mplstyle")
 
 DTYPE = jnp.float64
 
 
-def loss_fn(model, params, X, Y, M):
+def loss_fn(model, params, X, Y, M, target_token_idx):
     _, probs = model(params, X, attention_mask=M)
 
     seq_loss_fnc = jax.vmap(cross_entropy_loss)
@@ -34,7 +36,12 @@ def loss_fn(model, params, X, Y, M):
 
     batch_loss = jnp.sum(batch_loss_fnc(Y, probs))
 
-    return batch_loss
+    y_pred_one_hots = one_hot_encode(probs)
+    acc = accuracy(
+        Y[:, target_token_idx, :], y_pred_one_hots[:, target_token_idx, :]
+    )
+
+    return batch_loss, {"accuracy": acc}
 
 
 @hydra.main(version_base=None, config_path="conf", config_name="config")
@@ -85,93 +92,128 @@ def main(config: DictConfig) -> None:
     ckpt_path = Path() / config.checkpoints_path
     options = ocp.CheckpointManagerOptions(max_to_keep=2, create=True)
     manager = ocp.CheckpointManager(ckpt_path.absolute(), options=options)
+    history_path = Path() / config.checkpoints_path / "history.json"
 
     log.info("Initilizing model params...")
     vocab_size = tokenizer.vocab_size
     params = init_param_state(config, vocab_size, keygen, DTYPE)
-    if manager.latest_step() is not None:
+    history = {
+        "loss": {"train": [], "validation": []},
+        "metrics": {"accuracy": {"train": [], "validation": []}},
+    }
+    if manager.latest_step() is not None and config.use_checkpoint:
         log.info(
             f"Found checkpoint at step {manager.latest_step()}. Loading..."
         )
-        restored = manager.restore(
+        restored_params = manager.restore(
             manager.latest_step(), args=ocp.args.StandardRestore(params)
         )
-        params = restored
+        params = restored_params
+
+        with open(history_path) as f:
+            history = json.load(f)
 
     model = ft.partial(decoder_only, config=config, key=next(keygen))
 
     log.info("Starting training...")
     lr = config.learning_rate
     batch_size = config.batch_size
-    train_losses = []
-    val_losses = []
     for _ in tqdm(range(config.n_train_steps)):
         # Train
         step_train_loss = 0
-        num_train_batches = len(X_train) // batch_size
+        step_train_accuracy = 0
+        num_train_samples = len(X_train)
+        num_train_batches = (
+            num_train_samples // batch_size + num_train_samples % batch_size
+        )
         for batch in range(num_train_batches):
             batch_slice = slice(batch * batch_size, (batch + 1) * batch_size)
             X_batch = X_train[batch_slice]
             Y_batch = Y_train[batch_slice]
             M_batch = M_train[batch_slice]
 
-            batch_train_loss, grad = jax.value_and_grad(loss_fn, argnums=1)(
-                model, params, X_batch, Y_batch, M_batch
+            grad_fn = jax.value_and_grad(loss_fn, argnums=1, has_aux=True)
+
+            (batch_train_loss, batch_train_metrics), grad = grad_fn(
+                model, params, X_batch, Y_batch, M_batch, target_token_idx
             )
 
             step_train_loss += batch_train_loss
+            step_train_accuracy += (
+                batch_train_metrics["accuracy"] / num_train_batches
+            )
 
             # TODO: Implement Adam instead of SGD
             params = jax.tree.map(lambda w, d: w - lr * d, params, grad)
 
         # Validate
         step_val_loss = 0
-        num_val_batches = len(X_val) // batch_size
+        step_val_accuracy = 0
+        num_val_samples = len(X_val)
+        num_val_batches = (
+            num_val_samples // batch_size + num_train_samples % batch_size
+        )
         for batch in range(num_val_batches):
             batch_slice = slice(batch * batch_size, (batch + 1) * batch_size)
             X_batch = X_val[batch_slice]
             Y_batch = Y_val[batch_slice]
             M_batch = M_val[batch_slice]
 
-            batch_val_loss = loss_fn(model, params, X_val, Y_val, M_val)
+            batch_val_loss, batch_val_metrics = loss_fn(
+                model, params, X_val, Y_val, M_val, target_token_idx
+            )
 
             step_val_loss += batch_val_loss
+            step_val_accuracy += (
+                batch_val_metrics["accuracy"] / num_val_batches
+            )
 
-        train_losses.append(step_train_loss)
-        val_losses.append(step_val_loss)
+        history["loss"]["train"].append(float(step_train_loss))
+        history["loss"]["validation"].append(float(step_val_loss))
+        history["metrics"]["accuracy"]["train"].append(
+            float(step_train_accuracy)
+        )
+        history["metrics"]["accuracy"]["validation"].append(
+            float(step_val_accuracy)
+        )
 
-    # TODO: Need to figure out a way to save the lossess as well for plotting
-
-    log.info("Saving current model params...")
-    save_args = orbax_utils.save_args_from_target(params)
-    manager.save(
-        step=1, args=ocp.args.StandardSave(params, save_args=save_args)
-    )
+    if config.save_checkpoint:
+        log.info("Saving current model params...")
+        save_args = orbax_utils.save_args_from_target(params)
+        manager.save(
+            step=1, args=ocp.args.StandardSave(params, save_args=save_args)
+        )
+        with open(history_path, "w") as f:
+            json.dump(history, f)
 
     log.info("Saving losses plot...")
-    plt.plot(train_losses, label="train loss", marker="o")
-    plt.plot(val_losses, label="val loss", marker="o")
-    plt.yscale("log")
-    plt.xscale("log")
+    plt.title("Loss curves")
+    plt.plot(history["loss"]["train"], label="Train")
+    plt.plot(history["loss"]["validation"], label="Validation")
     plt.legend()
-    plt.savefig("history.png", dpi=300)
+    plt.savefig("loss_history.png")
+    plt.close()
+
+    plt.title("Accuracy curves")
+    plt.plot(history["metrics"]["accuracy"]["train"], label="Train")
+    plt.plot(history["metrics"]["accuracy"]["validation"], label="Validation")
+    plt.legend()
+    plt.savefig("accuracy_history.png")
     plt.close()
 
     log.info("Evaluating model...")
-    train_acc, train_cm = evaluate(
-        model, params, tokenizer, X_train, Y_train, M_train, target_token_idx
+    train_acc = evaluate(
+        model, params, X_train, Y_train, M_train, target_token_idx
     )
-    test_acc, test_cm = evaluate(
-        model, params, tokenizer, X_test, Y_test, M_test, target_token_idx
+    test_acc = evaluate(
+        model, params, X_test, Y_test, M_test, target_token_idx
     )
 
     log.info("Train performance:")
     log.info(f"Accuracy = {train_acc}")
-    log.info(f"Confusion Matrix: \n {train_cm}")
 
     log.info("Test performance:")
     log.info(f"Accuracy = {test_acc}")
-    log.info(f"Confusion Matrix: \n {test_cm}")
 
 
 if __name__ == "__main__":
--- src/grokking/model/utils.py
+++ src/grokking/model/utils.py
@@ -1,36 +1,30 @@
 import jax
 import jax.numpy as jnp
 
-def evaluate(model, params, tokenizer, X, Y, M, target_token_idx):
+
+def accuracy(y_true, y_pred):
+    """Per token accuracy
+
+    Expects 2D arrays with shape (batch_size, vocab_size)
+
+    """
+    return jnp.mean(jnp.all(y_true == y_pred, axis=1))
+
+def one_hot_encode(y_pred):
+    target_indice = jnp.argmax(y_pred, axis=-1)
+    num_classes = y_pred.shape[-1]
+    y_pred_one_hots = jax.nn.one_hot(target_indice, num_classes=num_classes)
+
+    return y_pred_one_hots
+
+def evaluate(model, params, X, Y, M, target_token_idx):
     # TODO: Make this batched inference (consider creating a batch inference function)
-    _, y_hat = model(params=params, x=X, attention_mask=M)
-    target_indice = jnp.argmax(y_hat, axis=-1)
-    num_classes = y_hat.shape[-1]
-    y_hat_one_hots = jax.nn.one_hot(target_indice, num_classes=num_classes)
+    _, y_pred = model(params=params, x=X, attention_mask=M)
+    y_pred_one_hots = one_hot_encode(y_pred)
 
-    y_hat = tokenizer.detokenize(y_hat_one_hots.tolist())
-    y_true = tokenizer.detokenize(Y.tolist())
+    y_true = Y[:, target_token_idx, :]
+    y_pred = y_pred_one_hots[:, target_token_idx, :]
 
-    y_trues = []
-    y_preds = []
-    correct_preds = 0
-    all_preds = 0
+    acc = accuracy(y_true, y_pred)
 
-    for i in range(len(Y)):
-        true = int(y_true[i][target_token_idx])
-        pred = int(y_hat[i][target_token_idx])
-
-        y_trues.append(true)
-        y_preds.append(pred)
-
-        if y_true[i][target_token_idx] == y_hat[i][target_token_idx]:
-            correct_preds += 1
-
-        all_preds += 1
-
-    cm = jnp.zeros((num_classes, num_classes), dtype=jnp.int32)
-    cm = cm.at[y_trues, y_preds].add(1)
-
-    accuracy = correct_preds / all_preds
-
-    return accuracy, cm
+    return acc
